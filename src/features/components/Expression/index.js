84a806949d5c72fd61b25fbc51d3744516fd09db
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2022-03-31 00:14:28 +0100

    Expression Placeholder

---
+++ src/features/components/Expression/index.js
@@ -0,0 +1,8 @@
+import { Input } from "../Input";
+
+/**
+ * TO DO use this in return statements, if, right side of attributions, either side of *+-/
+ */
+export class Expression extends Input {
+  
+}
